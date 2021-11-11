6b4d692fc8385298116a6dbcfc18621b76609cb6
jackline <cheptanuiJackline@gmail.comgit config --global user.email cheptanuiJackline@gmail.com>
2021-11-11 17:07:35 +0300

    first commit

---
+++ stock/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.forms.fields import CharField
+
+
+category_choice = (
+	
+		('Dry goods', 'Dry goods'),
+		('Fruits & Vegetables', 'Fruits & Vegetables'),
+      	('Meat', 'Meat'),
+         	('Others', 'Others'),
+	)
+
+class Category(models.Model):
+	name = models.CharField(max_length=50, blank=True, null=True)
+	def __str__(self):
+		return self.name
+  
+
+
+class Stock(models.Model):
+   category = models.ForeignKey(Category, on_delete=models.CASCADE,blank=True)   
+   item_name=models.CharField(max_length=50, blank=True,null=True)
+   quantity=models.IntegerField(default='0', blank=True,null=True)
+   receive_quantity=models.IntegerField(default='0', blank=True,null=True)
+   received_by=models.CharField(max_length=50, blank=True,null=True)
+   issue_quantity=models.IntegerField(default='0', blank=True,null=True)
+   issued_to=models.CharField(max_length=50, blank=True,null=True)
+   reorder_level=models.IntegerField(default='0', blank=True,null=True)
+   timestamp=models.DateTimeField(auto_now_add=True,auto_now=False)
+   last_updated=models.DateTimeField(auto_now_add=False,auto_now=True)
+   # date=models.DateTimeField(auto_now_add=False,auto_now=False)
+ 
+   # export_to_csv=models.BooleanField(default=False)
+
+# Create your models here.
+
+   def __str__(self):
+      return self.item_name+" "+str(self.quantity)
+
+
+class StockHistory(models.Model):
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+	item_name = models.CharField(max_length=50, blank=True, null=True)
+	quantity = models.IntegerField(default='0', blank=True, null=True)
+	receive_quantity = models.IntegerField(default='0', blank=True, null=True)
+	receive_by = models.CharField(max_length=50, blank=True, null=True)
+	issue_quantity = models.IntegerField(default='0', blank=True, null=True)
+	issue_by = models.CharField(max_length=50, blank=True, null=True)
+	issue_to = models.CharField(max_length=50, blank=True, null=True)
+	phone_number = models.CharField(max_length=50, blank=True, null=True)
+	created_by = models.CharField(max_length=50, blank=True, null=True)
+	reorder_level = models.IntegerField(default='0', blank=True, null=True)
+	last_updated = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
+	timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
+ 
+ 
+class Menu(models.Model):
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+	item_name =models.CharField(max_length=50, blank=True, null=True)
+	quantity = models.IntegerField(default='0', blank=True, null=True)
+	
